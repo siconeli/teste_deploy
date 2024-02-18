a0ac36f40a5970b315d468ca30aa6217c70d06e7
siconeli <henrique.siconeli.f@hotmail.com>
2024-02-18 04:15:20 -0400

    up

--- apps/processo/apps.py
+++ apps/processo/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 class ProcessoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.processo'
-
-    # Para o tratamento de sinal de Login e Logout ser executado e registrar mensagem de log no arquivo de log 'processo.log'
-    def ready(self):
-        import apps.processo.signals
