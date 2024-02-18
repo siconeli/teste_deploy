3bd9e06fc73b7a9c23baaa8663c09330213d7cd4
siconeli <henrique.siconeli.f@hotmail.com>
2024-02-18 12:01:04 -0400

    up

--- webger23/settings.py
+++ webger23/settings.py
@@ -157,13 +157,13 @@ USE_TZ = True # Utilizado em modo de Desenvolvimento
 
 # Configuração dos arquivos estáticos: css, js, imagens
 STATIC_URL = '/static/' # Usado durante o desenvolvimento
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = str(BASE_DIR / 'staticfiles') # Usado durante a produção
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 #================================================================================================================
 # Arquivos de Media (Para salvar os arquivos em endereço local, na mesma máquina do código) (USAR DURANTE O DESENVOLVIMENTO)
