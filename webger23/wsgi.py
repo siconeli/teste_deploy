d726a4b95cc6cbad043c093416adabfdd98dddd6
siconeli <henrique.siconeli.f@hotmail.com>
2024-02-18 03:34:04 -0400

    up

--- webger23/wsgi.py
+++ webger23/wsgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
 # from dj_static import Cling, MediaCling # Usado durante a Produção.
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'webger23.settings'
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
  
 
